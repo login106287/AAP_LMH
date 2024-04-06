c347434577c667286fcaa75cc64b88f95b74f0d7
login106287 <tquanghuy2607@gmail.com>
2024-04-06 15:52:26 +0700

    CANDIES
    HSG Tỉnh Thanh Hóa 2022

---
+++ CANDIES/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vb vector<bool>
+#define pii pair<ll, ll>
+#define FOR(i,a,b,c) for (int i=(a),_b=(b);i<b;i+=c)
+#define fi first
+#define se second
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n, counts=0, v=0;
+    cin >> n;
+    int a[n+1];
+    vi value;
+    FOR(i,0,n,1) {
+        cin >> a[i];
+        if (a[i]%3==0&&a[i]!=0) {
+            counts++;
+            value.push_back(a[i]);
+        }
+    }
+
+    for (auto x:value) {
+        x-=counts;
+        v+=x;
+    }
+
+    cout << counts << endl << v/3 <<endl;
+
+    return 0;
+}
