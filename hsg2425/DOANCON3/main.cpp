7d877209b3ae2d53ddeafd67908c58a11f9e1259
login106287 <tquanghuy2607@gmail.com>
2024-05-15 11:22:09 +0700

    Ti22 2024

---
+++ hsg2425/DOANCON3/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define FOR(i,a,b,c) for (int i=a; i<b; i+=c)
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(false);
+    int n,s=0;
+    cin >> n;
+    int a[n][n];
+
+    FOR(i,0,n,1) {
+        FOR(j,0,n,1) {
+
+        }
+    }
+
+
+
+
+    return 0;
+}
