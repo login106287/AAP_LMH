83b4d14aac8aa13c499767af010761fee13338ba
login106287 <tquanghuy2607@gmail.com>
2024-06-22 10:24:09 +0700

    Choosing Teams

---
+++ Codeforces/Choosing Teams/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define vl vector<ll>
+#define fi first
+#define se second
+#define FOR(i,a,b,c) for (int i=a; i<b; i+=c)
+#define FOD(i,a,b,c) for (int i=a; i>b; i-=c)
+#define endl "\n"
+using namespace std;
+
+
+main() {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int n,k;
+    cin >> n >> k;
+
+    int a[n];
+
+    FOR(i,0,n,1) {
+        cin >> a[i];
+    }
+
+
+
+    return 0;
+}
